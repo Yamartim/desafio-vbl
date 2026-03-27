c824bf558852f69f49105292a8e10f9f3315d63a
Martim Lima <54243954+Yamartim@users.noreply.github.com>
2026-03-26 23:58:59 -0300

    docs: final small ajustment

--- Assets/Scripts/ApiTesterUI.cs
+++ Assets/Scripts/ApiTesterUI.cs
@@ -2,7 +2,7 @@ using TMPro;
 using UnityEngine;
 
 // UI class used for testing the API before the game starts
-public class ApiTester : MonoBehaviour
+public class ApiTesterUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text resultText;
     [SerializeField] private TMP_InputField urlInputField;
@@ -10,7 +10,6 @@ public class ApiTester : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //urlInputField.text = ApiRequester.ApiUrl;
         urlInputField.text = ApiRequester.apiUrl;
     }
 
@@ -19,7 +18,7 @@ public class ApiTester : MonoBehaviour
         ApiRequester.SetApiUrl(urlInputField.text);
 
         RequestResult result = await ApiRequester.GetRequest();
-        resultText.text = $"Last Request Result: \n{result.HttpResult}";
+        resultText.SetText($"Last Request Result: \n{result.HttpResult}");
 
         if (result.Success)
         {
